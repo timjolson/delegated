d56a91d67b5b92cddcf1675c0b648cf696ebd112
timjolson <tim.lsn@gmail.com>
2019-06-05 16:11:08 -0500

    Add module and tests. Update README

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # delegated
 python3 decorator, method proxy, attribute proxy to delegate tasks to a sub-object
+
+    delegated.attr_proxy  # property object, allows access to another object's attribute
+    delegated.method_proxy  # property method, allows access to another object's method
+
+    from delegated import delegated
+    delegated  # class, use as method decorator
+    
+    # methods
+    delegated.attribute   # returns attr_proxy
+    delegated.attributes  # returns [attr_proxy, ...]
+    delegated.method      # returns method_proxy
+    delegated.methods     # returns [method_proxy, ...]
+
+
+Use the decorator inside a class definition
+
+    @delegated('name_of_sub_component')
+    def same_name_as_sub_component_method(self): pass
+    
+    @delegated('name_of_sub_component', 'name_of_sub_component_method')
+    def whatever_method_name(self): pass
+    
+
+Main usage case:
+
+    from delegated import delegated
+    
+    class Sub():
+        '''Subordinate class'''
+        attr1 = 1    
+        ...   
+        attr99 = 99
+        
+        def method1(self, *args, **kwargs): pass
+        ...
+        def method99(self, *args, **kwargs): pass
+    
+    class Master():
+        '''Class or instance attributes and methods are supported'''
+        sub = Sub()
+        def __init__(self):
+            self.instance_sub = Sub()
+    
+    class ImplicitMaster(Master):
+        """Implicit delegation is simplest. Note: need '' final parameter
+        to automatically apply the delegation to the class."""
+        delegated.attributes('sub', 'attr1 attr2, ..., attr99', '')
+        delegated.methods('sub', 'method1, method2, ..., method99', '')
+
+    class ExplicitMaster(Master):
+        '''Explicit delegation allows for renaming and code completion for methods and attrs.'''
+        attr1, ..., attr99 = delegated.attributes('sub', 'attr1, ..., attr99')
+        method1, ..., method99 = delegated.methods('sub', 'method1, ..., method99')
+
+    class DecoratedMaster(Master):
+        """Decorating allows for renaming and code completion for methods.
+        Note: defined methods' parameter structure is ignored"""
+        # simple
+        @delegated('sub')
+        def method1(self):pass
+        
+        # renaming
+        @delegated('sub', 'method69')
+        def best_method(self):pass
+
+
+Additional usage:
+
+    # already instantiated objects can be used (always the same object, 
+    # instead of dynamic or instance-base)
+
+    class Master(Master):
+        sub = Sub()
+
+        @delegated(sub)
+        def method1(self): pass
+        
+        @delegated(sub, 'method2')
+        def class_object_method(self): pass
+
+        method3 = delegated.method(sub, 'method3')
+        attr1 = delegated.attribute(sub, 'attr1')
---
+++ delegated.py
@@ -0,0 +1,127 @@
+from inspect import stack as _stack
+
+
+class attr_proxy(property):
+    def __init__(self, worker, attr_name, supervisor=None):
+        if supervisor is not None:
+            if isinstance(supervisor, str) and supervisor == '':
+                supervisor = _stack()[1].frame.f_locals
+
+            if isinstance(supervisor, dict):
+                supervisorsetter = lambda k, v: supervisor.update({k: v})
+            else:
+                supervisorsetter = lambda value: setattr(supervisor, attr_name, value)
+        else:
+            supervisorsetter = lambda *a: None
+
+        def deller(master_instance):
+            delattr(master_instance, attr_name)
+
+        if isinstance(worker, str):
+            self.target = '.'.join([worker, attr_name])
+            getter, setter = self.str_getter_setter(worker, attr_name)
+
+        else:
+            self.target = '.'.join([type(worker).__qualname__, attr_name])
+            getter, setter = self.obj_getter_setter(worker, attr_name)
+
+        property.__init__(self, getter, setter, deller)
+
+        self.name = attr_name
+        self.worker = worker
+        supervisorsetter(attr_name, self)
+
+    def obj_getter_setter(self, worker, attr_name):
+        def getter(master_instance):
+            if isinstance(worker, dict): res = worker.get(attr_name)
+            else: res = getattr(worker, attr_name)
+            return res
+
+        def setter(master_instance, value):
+            if isinstance(worker, dict): worker.update({attr_name: value})
+            else: setattr(worker, attr_name, value)
+        return getter, setter
+
+    def str_getter_setter(self, worker, attr_name):
+        def getter(master_instance):
+            sub = getattr(master_instance, worker)
+            if isinstance(sub, dict): res = sub.get(attr_name)
+            else: res = getattr(sub, attr_name)
+            return res
+
+        def setter(master_instance, value):
+            sub = getattr(master_instance, worker)
+            if isinstance(sub, dict): sub.update({attr_name: value})
+            else: setattr(sub, attr_name, value)
+        return getter, setter
+
+    def __repr__(self):
+        return f"{type(self).__qualname__}({repr(self.worker)}, {self.name})"
+
+
+class method_proxy(attr_proxy):
+    def obj_getter_setter(self, worker, attr_name):
+        def getter(master_instance):
+            if isinstance(worker, dict): res = worker.get(attr_name)
+            else: res = getattr(worker, attr_name)
+            return res
+
+        def setter(master_instance, value):
+            setattr(master_instance, attr_name, value)
+
+        return getter, setter
+
+    def str_getter_setter(self, worker, attr_name):
+        def getter(master_instance):
+            sub = getattr(master_instance, worker)
+            if isinstance(sub, dict): res = sub.get(attr_name)
+            else: res = getattr(sub, attr_name)
+            return res
+
+        def setter(master_instance, value):
+            setattr(master_instance, attr_name, value)
+
+        return getter, setter
+
+
+class delegated():
+    def __init__(self, worker, attr_name=None):
+        self.worker = worker
+        self.attr_name = attr_name
+
+    def __call__(self, func):
+        return delegated.method(self.worker, self.attr_name or func.__name__)
+
+    class attribute(attr_proxy):
+        pass
+
+    @staticmethod
+    def attributes(worker, attrs, supervisor=None):
+        if isinstance(supervisor, str) and supervisor == '':
+            supervisor = _stack()[1].frame.f_locals
+
+        proxies = []
+        for attr_name in delegated.split(attrs):
+            attr_proxy = delegated.attribute(worker, attr_name, supervisor)
+            proxies.append(attr_proxy)
+        return proxies
+
+    class method(method_proxy):
+        pass
+
+    @staticmethod
+    def methods(worker, attrs, supervisor=None):
+        if isinstance(supervisor, str) and supervisor == '':
+            supervisor = _stack()[1].frame.f_locals
+
+        proxies = []
+        for attr_name in delegated.split(attrs):
+            method_proxy = delegated.method(worker, attr_name, supervisor)
+            proxies.append(method_proxy)
+        return proxies
+
+    @staticmethod
+    def split(attrs):
+        if isinstance(attrs, str):
+            attrs = attrs.replace(',', ' ').split()
+        return attrs
