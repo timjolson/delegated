d849990479ef4be11fd38370adf02e155c818f14
timjolson <timjolson@users.noreply.github.com>
2020-09-09 23:09:52 -0500

    1. Combined proxies into delegated class directly, combined attrs and methods proxies. 1b. Cleaned up tests for these duplicates. 2. Split tests into granular parts. 3. Updated README.md . 4. Replaced .attributes/.methods with .tasks . 5. Replaced attribute/method classes with __proxy . 6. Eliminated usage of '' with `.here` .

--- delegated.py
+++ delegated.py
@@ -1,143 +1,147 @@
 from inspect import stack as _stack
+# TODO: test if we can use entire module as decorator (use __init__.py)
 
+class delegated(object):
+    """Class to delegate tasks to a subordinate object.
 
-class attr_proxy(property):
-    def __init__(self, worker, attr_name, supervisor=None):
-        if supervisor is not None:
-            if isinstance(supervisor, str) and supervisor == '':
-                supervisor = _stack()[1].frame.f_locals
+        Subordinates can be an existing object, or the name of an instance//class attribute
+        to be dynamically retrieved.
 
-            if isinstance(supervisor, dict):
-                supervisorsetter = lambda k, v: supervisor.update({k: v})
-            else:
-                supervisorsetter = lambda k, value: setattr(supervisor, k, value)
-        else:
-            supervisorsetter = lambda *a: None
+        Supervisor can be a class instance, or a dict (keys are used as attribute names).
+    """
 
-        def deller(supervisor_instance):
-            delattr(supervisor_instance, attr_name)
-
-        self.name = attr_name
-        self.worker = worker.split('.') if isinstance(worker, str) else worker
-
-        if isinstance(worker, str):
-            self.target = '.'.join([worker, attr_name])
-            getter = self.str_getter(self.worker, attr_name)
-            setter = self.str_setter(self.worker, attr_name)
-        else:
-            self.target = '.'.join([type(worker).__qualname__, attr_name])
-            getter, setter = self.obj_getter_setter(self.worker, attr_name)
-
-        property.__init__(self, getter, setter, deller)
-
-        supervisorsetter(attr_name, self)
-
-    def obj_getter_setter(self, worker, attr_name):
-        def getter(supervisor_instance):
-            res = getattr(worker, attr_name)
-            return res
-
-        def setter(supervisor_instance, value):
-            setattr(worker, attr_name, value)
-        return getter, setter
-
-    def str_getter(self, worker, attr_name):
-        def getter(supervisor_instance):
-            trace = [type(supervisor_instance).__name__]
-            try:
-                trace.append(worker[0])
-                sub = getattr(supervisor_instance, worker[0])
-                for w in worker[1:]:
-                    trace.append(w)
-                    if w.endswith('()'):
-                        sub = getattr(sub, w[:-2])()
-                    else:
-                        sub = getattr(sub, w)
-                res = getattr(sub, attr_name)
-            except AttributeError:
-                raise AttributeError(f"'{'.'.join(map(str, trace))}' object has no attribute '{attr_name}'")
-            return res
-        return getter
-
-    def str_setter(self, worker, attr_name):
-        def setter(supervisor_instance, value):
-            sub = getattr(supervisor_instance, worker[0])
-            for w in worker[1:]:
-                if w.endswith('()'):
-                    sub = getattr(sub, w[:-2])()
-                else:
-                    sub = getattr(sub, w)
-            setattr(sub, attr_name, value)
-        return setter
-
-    def __repr__(self):
-        return f"{type(self).__qualname__}({repr(self.worker)}, {self.name})"
-
-
-class method_proxy(attr_proxy):
-    def str_setter(self, worker, attr_name):
-        def setter(supervisor_instance, value):
-            setattr(supervisor_instance, attr_name, value)
-        return setter
-
-    def obj_getter_setter(self, worker, attr_name):
-        def getter(supervisor_instance):
-            res = getattr(worker, attr_name)
-            return res
-
-        def setter(supervisor_instance, value):
-            setattr(supervisor_instance, attr_name, value)
-        return getter, setter
-
-
-class delegated():
     def __init__(self, worker, attr_name=None):
+        # Runs when decorating
         self.worker = worker
         self.attr_name = attr_name
 
     def __call__(self, func):
-        return delegated.method(self.worker, self.attr_name or func.__name__)
+        # Runs when decorating, after __init__ (wraps function)
+        return delegated.__proxy(self.worker, self.attr_name or func.__name__)
 
-    class attribute(attr_proxy):
-        pass
+    class __proxy(property):
+        # Builds a property object to handle proxy-ing
+        def __init__(self, worker, attr_name, supervisor=None):
+            if supervisor is not None:
+                if isinstance(supervisor, dict):
+                    def supervisor_setter(k, v):
+                        return supervisor.update({k: v})
+                else:
+                    def supervisor_setter(k, v):
+                        return setattr(supervisor, k, v)
+
+                def deller(supervisor_instance):
+                    delattr(supervisor_instance, attr_name)
+            else:
+                def supervisor_setter(*a, **k):
+                    return None
+
+                def deller(supervisor_instance):
+                    pass
+
+            self.attr_name = attr_name
+            self.worker = worker.split('.') if isinstance(worker, str) else worker
+
+            if isinstance(worker, str):
+                self.target = '.'.join([worker, attr_name])
+                getter = self.str_getter(self.worker, attr_name)
+                setter = self.str_setter(self.worker, attr_name)
+            else:
+                self.target = '.'.join([type(worker).__qualname__, attr_name])
+                getter, setter = self.obj_getter_setter(self.worker, attr_name)
+
+            property.__init__(self, getter, setter, deller)
+
+            supervisor_setter(attr_name, self)
+
+        def obj_getter_setter(self, worker, attr_name):
+            def getter(supervisor_instance):
+                res = getattr(worker, attr_name)
+                return res
+
+            def setter(supervisor_instance, value):
+                setattr(worker, attr_name, value)
+
+            return getter, setter
+
+        def str_getter(self, worker, attr_name):
+            def getter(supervisor_instance):
+                trace = [type(supervisor_instance).__name__]
+                try:
+                    trace.append(worker[0])
+                    sub = getattr(supervisor_instance, worker[0])
+                    for w in worker[1:]:
+                        trace.append(w)
+                        if w.endswith('()'):
+                            sub = getattr(sub, w[:-2])()
+                        else:
+                            sub = getattr(sub, w)
+                    res = getattr(sub, attr_name)
+                except AttributeError:
+                    raise AttributeError(f"'{'.'.join(map(str, trace))}' object has no attribute '{attr_name}'")
+                return res
+
+            return getter
+
+        def str_setter(self, worker, attr_name):
+            def setter(supervisor_instance, value):
+                sub = getattr(supervisor_instance, worker[0])
+                for w in worker[1:]:
+                    if w.endswith('()'):
+                        sub = getattr(sub, w[:-2])()
+                    else:
+                        sub = getattr(sub, w)
+                setattr(sub, attr_name, value)
+
+            return setter
+
+        def __repr__(self):
+            return f"{type(self).__qualname__}({repr(self.worker)}, {repr(self.attr_name)})"
 
     @staticmethod
-    def attributes(worker, attrs, supervisor=None):
-        if isinstance(supervisor, str) and supervisor == '':
-            supervisor = _stack()[1].frame.f_locals
+    def tasks(worker, attrs, supervisor=None):
+        """Delegates attribute(s) and returns their proxies.
 
+        :param worker: str or object; subordinate(s) to delegate to
+        :param attrs: str or sequence of strings; attributes to delegate
+        :param supervisor: the delegating object or `None`
+        :return: a proxy (if one is requested), or list of proxies (if multiple)
+        """
         proxies = []
         attrs = delegated.split(attrs)
 
         if len(attrs) == 1:
-            return delegated.attribute(worker, attrs[0], supervisor)
+            return delegated.__proxy(worker, attrs[0], supervisor)
 
         for attr_name in attrs:
-            attr_proxy = delegated.attribute(worker, attr_name, supervisor)
+            attr_proxy = delegated.__proxy(worker, attr_name, supervisor)
             proxies.append(attr_proxy)
         return proxies
 
-    class method(method_proxy):
-        pass
-
     @staticmethod
-    def methods(worker, attrs, supervisor=None):
-        if isinstance(supervisor, str) and supervisor == '':
-            supervisor = _stack()[1].frame.f_locals
+    def here(worker, attrs):
+        """Delegates attribute(s) and embeds into the containing class.
+
+        :param worker: str or object; subordinate(s) to delegate to
+        :param attrs: str or sequence of strings; attributes to delegate
+        :return: a proxy (if one is requested), or list of proxies (if multiple)
+        """
+        supervisor = _stack()[1].frame.f_locals
 
         proxies = []
         attrs = delegated.split(attrs)
 
         if len(attrs) == 1:
-            return delegated.method(worker, attrs[0], supervisor)
+            return delegated.__proxy(worker, attrs[0], supervisor)
 
         for attr_name in attrs:
-            method_proxy = delegated.method(worker, attr_name, supervisor)
-            proxies.append(method_proxy)
+            attr_proxy = delegated.__proxy(worker, attr_name, supervisor)
+            proxies.append(attr_proxy)
         return proxies
 
     @staticmethod
     def split(attrs):
+        # Split task strings by comma or space
         if isinstance(attrs, str):
             attrs = attrs.replace(',', ' ').split()
         return attrs
