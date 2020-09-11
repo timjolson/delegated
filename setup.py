0082810d2b4a767df8d501d18d209f33f32ced3b
timjolson <timjolson@users.noreply.github.com>
2020-09-10 22:29:07 -0500

    Started setup for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
 setup(
     name='delegated',
-    version="1.0",
+    version="0.8",
     author="timjolson",
     author_email="timjolson@users.noreply.github.com",
-    description="Utility to quickly delegate methods and attributes to subordinate objects.",
+    description="Utility to quickly delegate attributes to subordinate objects via proxy (property).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='GPLv3',
     url="https://github.com/timjolson/delegated",
     packages=find_packages(),
     tests_require=['pytest'],
+    python_requires='>=3.0',
+    classifiers=[  # https://pypi.org/classifiers/
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ]
 )
 
 
@@ -26,10 +39,29 @@ setup(
 #     long_description_content_type="text/markdown",
 #     url="https://github.com/pypa/sampleproject",
 #     packages=find_packages(),
-#     classifiers=[
-#         "Programming Language :: Python :: 3",
-#         "License :: OSI Approved :: MIT License",
-#         "Operating System :: OS Independent",
-#     ],
 #     python_requires='>=3.6',
+#     classifiers=[
+#           # How mature is this project? Common values are
+#           #   3 - Alpha
+#           #   4 - Beta
+#           #   5 - Production/Stable
+#           'Development Status :: 3 - Alpha',
+#
+#           # Indicate who your project is intended for
+#           'Intended Audience :: Developers',
+#           'Topic :: Software Development :: Build Tools',
+#
+#           # Pick your license as you wish (should match "license" above)
+#           'License :: OSI Approved :: MIT License',
+#
+#           # Specify the Python versions you support here. In particular, ensure
+#           # that you indicate whether you support Python 2, Python 3 or both.
+#           'Programming Language :: Python :: 2',
+#           'Programming Language :: Python :: 2.6',
+#           'Programming Language :: Python :: 2.7',
+#           'Programming Language :: Python :: 3',
+#           'Programming Language :: Python :: 3.2',
+#           'Programming Language :: Python :: 3.3',
+#           'Programming Language :: Python :: 3.4',
+#           ],
 # )
